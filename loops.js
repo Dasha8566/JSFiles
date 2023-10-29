88ec3a538016e6c87f37e25056b71c314c5d5dee
Daria Orlova <orlovad1802@gmail.com>
2023-10-30 00:47:41 +0200

    Added Features

--- loops.js
+++ loops.js
@@ -1,8 +1,10 @@
-const test = [56,34,11,10,74];
+const test = [56,34,11,10,74,81];
 
 function search(arr,target){
     for (let i=0; i<arr.length;i++){
         if(arr[i] == target) return i;
     } return -1;
 }
-console.log(search(test,10));
+
+console.log(search(test,56));
+console.log(search(test,74));
