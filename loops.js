c6e0682f34a2ec506a1559beca30c2a51b6003bd
Daria Orlova <orlovad1802@gmail.com>
2023-10-30 00:38:09 +0200

    First Commit

---
+++ loops.js
@@ -0,0 +1,8 @@
+const test = [56,34,11,10,74];
+
+function search(arr,target){
+    for (let i=0; i<arr.length;i++){
+        if(arr[i] == target) return i;
+    } return -1;
+}
+console.log(search(test,10));
